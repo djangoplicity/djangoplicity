d37213a683424664e9370be59f1406003999afc9
Andrés Linares <andres.linares.971122@gmail.com>
2020-06-18 00:29:49 -0500

    Feature/initial tests + CI with Github Actions (#2)
    * basic docker and django setup
    
    * using postgres as db
    
    * adding test dependencies
    
    * adding tests folder
    
    * copying test and added makefile
    
    * improve dockerfile
    
    * changing volumes
    
    * add initially required files of django
    
    * trying first test
    
    * add test
    
    * remove test
    
    * adding settings
    
    * using command dev
    
    * copying setup.cfg
    
    * add celery
    
    * adding required files to setup
    
    * create django tests workflow
    
    * config venv and editorconfig
    
    * add test project
    
    * add requirements
    
    * temp disable not working tests
    
    * only python 2.7
    
    * multiple python versions and disable fail-fast
    
    * remove not required celery config
    
    * checking tests
    
    * verbose pytest
    
    * added tox generated files to gitignore
    
    * splitting requirements like django-rest-framework
    
    * setup tox.ini with only py27-django111
    
    * temporally testing
    
    * setup tox ini agains multiple environments
    
    * added make command
    
    * renamed test to use pytest
    
    * showing coverage as html
    
    * more tests working (renamed)
    
    * more tests
    
    * added timeout dependency
    
    * language code to en
    
    * remove jquery files
    
    * rename reports tests
    
    * recover reports tests content
    
    * rename djangoplicity tests
    
    * recover translations tests name
    
    * recover test translation content
    
    * recover utils tests
    
    * link requirements.txt to requirements folder
    
    * run only python 2.7 in CI for now
    
    * test if utils is running
    
    * useful docker
    
    * docker commands and docker working
    
    * usign only django unittests
    
    * fixes
    
    * utils tests working
    
    * add djangoplicity global tests
    
    Co-authored-by: Edison Arango <edisonarab@gmail.com>

--- djangoplicity/utils/tests.py
+++ djangoplicity/utils/tests.py
@@ -6,41 +6,43 @@
 #   Luis Clara Gomes <lcgomes@eso.org>
 #
 
-try:
-    from django.utils import unittest
-except ImportError:
-    import unittest
+from djangoplicity.utils import datetimes
 from datetime import datetime
+from django.test import TestCase
+from django.conf import settings
 
-
-class TemplateTagDatetimeTest( unittest.TestCase ):
-
-    #def test_timezone(self):
-    #   response = self.client.get('/customer/details/')
-    #   self.assertEqual(response.status_code, 200)
+class TestDatetimes(TestCase):
+    def test_timestring_to_seconds(self):
+        """ test correct conversion of H:m:s:f to seconds """
+        time = '10:30:40:0'
+        seconds = (10 * 60 * 60) + (30 * 60) + 40
+        
+        calculated_seconds = datetimes.timestring_to_seconds(time)
+        assert seconds == calculated_seconds
 
     def test_timezone(self):
-        from django.conf import settings
+        """ test conversion of local time to specific timezone """
         settings.TIME_ZONE = 'Europe/Berlin'
+
         from djangoplicity.utils.templatetags.djangoplicity_datetime import timezone
 
-        est = datetime(2011, 3, 10, 12, 00 )
-        edt = datetime(2011, 3, 15, 12, 00 )
-        cet = datetime(2011, 3, 24, 12, 00 )
-        cest = datetime(2011, 3, 30, 12, 00 )
+        est = datetime(2011, 3, 10, 12, 00 ) # Eastern Standard Time
+        edt = datetime(2011, 3, 15, 12, 00 ) # Eastern Daylight Time
+        cet = datetime(2011, 3, 24, 12, 00 ) # Central European Time
+        cest = datetime(2011, 3, 30, 12, 00 ) # Central European Summer Time
 
-        self.assertEqual( timezone( est, 'Europe/Berlin' ).isoformat(), '2011-03-10T12:00:00+01:00' )
-        self.assertEqual( timezone( edt, 'Europe/Berlin' ).isoformat(), '2011-03-15T12:00:00+01:00' )
-        self.assertEqual( timezone( cet, 'Europe/Berlin' ).isoformat(), '2011-03-24T12:00:00+01:00' )
-        self.assertEqual( timezone( cest, 'Europe/Berlin' ).isoformat(), '2011-03-30T12:00:00+02:00' )
 
-        self.assertEqual( timezone( est, 'US/Eastern' ).isoformat(), '2011-03-10T06:00:00-05:00' )
-        self.assertEqual( timezone( edt, 'US/Eastern' ).isoformat(), '2011-03-15T07:00:00-04:00' )
-        self.assertEqual( timezone( cet, 'US/Eastern' ).isoformat(), '2011-03-24T07:00:00-04:00' )
-        self.assertEqual( timezone( cest, 'US/Eastern' ).isoformat(), '2011-03-30T06:00:00-04:00' )
+        assert timezone(est, 'Europe/Berlin').isoformat() == '2011-03-10T12:00:00+01:00'
+        assert timezone(edt, 'Europe/Berlin').isoformat() == '2011-03-15T12:00:00+01:00'
+        assert timezone(cet, 'Europe/Berlin').isoformat() == '2011-03-24T12:00:00+01:00'
+        assert timezone(cest, 'Europe/Berlin').isoformat() == '2011-03-30T12:00:00+02:00'
 
-    def test_time4lang( self ):
-        from django.conf import settings
+        assert timezone(est, 'US/Eastern').isoformat() == '2011-03-10T06:00:00-05:00'
+        assert timezone(edt, 'US/Eastern').isoformat() == '2011-03-15T07:00:00-04:00'
+        assert timezone(cet, 'US/Eastern').isoformat() == '2011-03-24T07:00:00-04:00'
+        assert timezone(cest, 'US/Eastern').isoformat() == '2011-03-30T06:00:00-04:00'
+
+    def test_time4lang(self):
         settings.TIME_ZONE = 'Europe/Berlin'
         settings.LANGUAGE_TIMEZONE = {
             'pt-br': 'America/Sao_Paulo',
@@ -59,21 +61,16 @@ class TemplateTagDatetimeTest( unittest.TestCase ):
         # CEST -> CET: 2011/10/30
         # Diff: -3
 
-        brst = datetime(2011, 2, 18, 12, 00)
-        brt = datetime(2011, 2, 22, 12, 00)
-        cet = datetime(2011, 3, 24, 12, 00)
-        cest = datetime(2011, 3, 30, 12, 00)
+        brst = datetime(2011, 2, 18, 12, 00) # Brasilia Summer Time
+        brt = datetime(2011, 2, 22, 12, 00) # Brasilia Time
+        cet = datetime(2011, 3, 24, 12, 00) # Central European Time
+        cest = datetime(2011, 3, 30, 12, 00) # Central European Summer Time
         cest_brst = datetime(2011, 10, 18, 12, 00)
         cet_brst = datetime(2011, 11, 01, 12, 00)
 
-        self.assertEqual( time4lang(brst, 'pt-br').isoformat(), '2011-02-18T09:00:00-02:00')
-        self.assertEqual( time4lang(brt, 'pt-br').isoformat(), '2011-02-22T08:00:00-03:00')
-        self.assertEqual( time4lang(cet, 'pt-br').isoformat(), '2011-03-24T08:00:00-03:00')
-        self.assertEqual( time4lang(cest, 'pt-br').isoformat(), '2011-03-30T07:00:00-03:00')
-        self.assertEqual( time4lang(cest_brst, 'pt-br').isoformat(), '2011-10-18T08:00:00-02:00')
-        self.assertEqual( time4lang(cet_brst, 'pt-br').isoformat(), '2011-11-01T09:00:00-02:00')
-
-
-if __name__ == "__main__":
-    import unittest
-    unittest.main()
+        assert time4lang(brst, 'pt-br').isoformat() == '2011-02-18T09:00:00-02:00'
+        assert time4lang(brt, 'pt-br').isoformat() == '2011-02-22T08:00:00-03:00'
+        assert time4lang(cet, 'pt-br').isoformat() == '2011-03-24T08:00:00-03:00'
+        assert time4lang(cest, 'pt-br').isoformat() == '2011-03-30T07:00:00-03:00'
+        assert time4lang(cest_brst, 'pt-br').isoformat() == '2011-10-18T08:00:00-02:00'
+        assert time4lang(cet_brst, 'pt-br').isoformat() == '2011-11-01T09:00:00-02:00'
