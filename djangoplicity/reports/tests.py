3dd64927216294b60c0ba4276450b6073054c7bc
andres-linares <andres.linares.971122@gmail.com>
2020-06-18 15:20:43 -0500

    testing engine

--- djangoplicity/reports/tests.py
+++ djangoplicity/reports/tests.py
@@ -8,6 +8,9 @@
 from django.test import TestCase
 from djangoplicity.test.testcases import AdminTestCase
 
+from models import Report, ReportGroup
+from engine import ReportEngine, ReportExecutionError, ReportResult
+
 
 class ReportsAdminTestCase(AdminTestCase):
     fixtures = ['reports.json']
@@ -15,47 +18,54 @@ class ReportsAdminTestCase(AdminTestCase):
     def test_report_access(self):
         response = self.client.get('/admin/reports/report/')
         self.assertEqual(response.status_code, 200)
-        response = self.client.get('/admin/reports/report/1')
-        self.assertEqual(response.status_code, 301)
+        response = self.client.get('/admin/reports/report/1/')
+        self.assertEqual(response.status_code, 302)
+
+    def test_report_redirect(self):
+        """
+        Tests that the redirection is actually triggering report view
+        """
+        response = self.client.get('/admin/reports/report/1/', follow=True)
+        # TODO: Finish
 
     def test_reportgroup_access(self):
         response = self.client.get('/admin/reports/reportgroup/')
         self.assertEqual(response.status_code, 200)
-        response = self.client.get('/admin/reports/reportgroup/1')
-        self.assertEqual(response.status_code, 301)
-
+        response = self.client.get('/admin/reports/reportgroup/1/')
+        self.assertEqual(response.status_code, 302)
 
 
 class ReportEngineTestCase(TestCase):
     fixtures = ['reports.json']
 
-    def test_report_engine(self):
-        from engine import *
-        from models import Report
-
+    def test_report_engine_exceptions(self):
         report_empty = Report.objects.get(pk=2)
-        report_bad_fields = Report.objects.get(pk=3)
+        report_empty_fields = Report.objects.get(pk=3)
         report_bad_sql = Report.objects.get(pk=4)
+        report_too_many_fields = Report.objects.get(pk=5)
 
         self.failUnlessRaises(ReportExecutionError,
                               ReportEngine.run_report, report_empty)
-        # self.failUnlessRaises(ReportExecutionError, ReportEngine.run_report, report_bad_fields) # Legacy tests were not even working xD
+        self.failUnlessRaises(ReportExecutionError,
+                              ReportEngine.run_report, report_empty_fields)
         self.failUnlessRaises(ReportExecutionError,
                               ReportEngine.run_report, report_bad_sql)
+        self.failUnlessRaises(ReportExecutionError,
+                              ReportEngine.run_report, report_too_many_fields)
 
+    def test_report_engine(self):
+        group = ReportGroup.objects.create(name='Grouped Reports')
+        report = Report.objects.create(
+            name='Grouped Report Names',
+            group_id=group.id,
+            description='Life is wonderful',
+            displayed_fields='Name',
+            sql_command='SELECT name FROM reports_reportgroup'
+        )
 
-def report_engine_test():
-    """	
-    >>> from reports.engine import ReportEngine	
-    >>> from reports.models import *	
-    >>> group = ReportGroup.objects.create(name='Statistics')	
-    >>> group.save()	
-    >>> report = Report.objects.create(name='Good report', group_id=group.id, description='Some description', displayed_fields='Name', sql_command='SELECT name FROM reports_reportgroup' )	
-    >>> report.save()	
-    >>> result = ReportEngine.run_report( report )	
-    >>> result.report	
-    <Report: Good report>	
-    >>> result.rows	
-    ((u'Statistics',), (u'Statistics',))	
-    """
-    pass
+        result = ReportEngine.run_report(report)
+
+        self.assertEqual(result.report, report)
+        
+        all_reports = [(u'Statistics',), (u'Grouped Reports',)]
+        self.assertEqual(result.rows, all_reports)
