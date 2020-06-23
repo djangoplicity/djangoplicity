a0769265e6edaa060d11611b145461544d305af7
Edison Arango <edisonarab@gmail.com>
2020-06-23 11:59:51 -0500

    add, archives and celery config

--- test_project/dp_settings.py
+++ test_project/dp_settings.py
@@ -4,9 +4,24 @@ from .settings import *
 SHORT_NAME = 'Djangoplicity'
 TMP_DIR = os.path.join(BASE_DIR, 'tmp')
 
-# DP APPS
-INSTALLED_APPS += [
+# APPLICATION DEFINITION
+DJANGO_APPS = [
+    # Do not autodiscover admin.py modules in all apps because test_project.admin configures custom Admin Sites itself
+    # See: https://docs.djangoproject.com/en/1.10/ref/contrib/admin/#discovery-of-admin-files
+    'django.contrib.admin.apps.SimpleAdminConfig',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    # Manage multiple domains content
     'django.contrib.sites',
+    # Redirect URLs
+    'django.contrib.redirects',
+]
+
+DJANGOPLICITY_APPS = [
+    # Djangoplicity is an app package itself containing main templates, statics, etc
     'djangoplicity',
     'djangoplicity.menus',
     'djangoplicity.pages',
@@ -19,6 +34,14 @@ INSTALLED_APPS += [
     'djangoplicity.utils',
 ]
 
+THIRD_PARTY_APPS = [
+    # WYSIWYG HTML Editor (Used for example in pages editing)
+    'tinymce',
+]
+
+INSTALLED_APPS = DJANGO_APPS + DJANGOPLICITY_APPS + THIRD_PARTY_APPS
+
+# SITES
 SITE_ID=1
 
 if USE_I18N:
@@ -54,10 +77,31 @@ LANGUAGES = [
 ]
 
 # ARCHIVES
+ARCHIVE_IMPORT_ROOT = os.path.join(BASE_DIR, 'import')
 ARCHIVE_ROOT = 'archives/'
 IMAGES_ARCHIVE_ROOT = 'archives/images/'
 VIDEOS_ARCHIVE_ROOT = 'archives/videos/'
 RELEASE_ARCHIVE_ROOT = 'archives/releases/'
+ARCHIVE_AUTO_RESOURCE_DELETION = False
+
+ARCHIVES = (
+    ('djangoplicity.media.models.Image', 'djangoplicity.media.options.ImageOptions'),
+    ('djangoplicity.media.models.Video', 'djangoplicity.media.options.VideoOptions'),
+    ('djangoplicity.media.models.VideoSubtitle', 'djangoplicity.media.options.VideoSubtitleOptions'),
+    ('djangoplicity.media.models.ImageComparison', 'djangoplicity.media.options.ImageComparisonOptions'),
+)
 
 # Allows templates coverage
 TEMPLATES[0]['OPTIONS']['debug'] = True
+
+# CELERY
+# Task result backend
+CELERY_RESULT_BACKEND = "amqp"
+CELERY_BROKER_URL = 'amqp://guest:guest@broker:5672/'
+# Avoid infinite wait times and retries
+CELERY_BROKER_TRANSPORT_OPTIONS = {
+    'max_retries': 3,
+    'interval_start': 0,
+    'interval_step': 0.2,
+    'interval_max': 0.2,
+}
