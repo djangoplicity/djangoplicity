639b1870c26079714e54f109d8c09c40d8c9df1d
andres-linares <andres.linares.971122@gmail.com>
2020-07-13 10:46:39 -0500

    tests

---
+++ djangoplicity/releases/tests.py
@@ -0,0 +1,35 @@
+from django.test import TestCase
+from djangoplicity.releases.options import ReleaseOptions
+
+class ReleasesTestCase(TestCase):
+    conf = {
+        'releases': {
+            'root': 'news',
+            'options': ReleaseOptions,
+            'list_views': [
+                ('search', '', 200),
+                ('staging', '', 200),
+                ('year', '2020/', 200),
+            ]
+        }
+    }
+
+    def test_index_root(self):
+        response = self.client.get('/news/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_queries(self):
+        for conf in self.conf.values():
+            options = conf['options']
+            root = conf['root']
+            views = conf['list_views']
+
+            for query, subpart, code in views:
+                view_url_root = "%sarchive/%s/%s" % (root, query, subpart)
+                response = self.client.get(view_url_root)
+                self.assertEqual(response.status_code, code)
+
+                for browser in getattr(opt.Queries, query).browsers:
+                    view_url = "%s%s/" % (view_url_root, browser)
+                    response = self.client.get(view_url)
+                    self.assertEqual(response.status_code, code)
