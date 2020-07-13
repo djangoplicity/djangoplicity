84b0766272c6cc0d4068ae611ce00287a8ecd76f
andres-linares <andres.linares.971122@gmail.com>
2020-07-13 10:21:39 -0500

    testing urls

---
+++ djangoplicity/iframe/tests.py
@@ -0,0 +1,14 @@
+from django.test import TestCase
+
+class IframeTestCase(TestCase):
+
+    urls = [
+        # '/facebook/welcome/',
+        '/facebook/discoveries/',
+        '/facebook/virtualtours/'
+    ]
+
+    def test_url_access(self):
+        for url in self.urls:
+            response = self.client.get(url)
+            self.assertEqual(response.status_code, 200)
