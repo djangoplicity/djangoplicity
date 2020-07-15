5df99d08eec2f75c49476143f240c2c8efeb01ae
Andrés Linares <andres.linares.971122@gmail.com>
2020-07-15 15:51:32 -0500

    Tests/releases (#31)
    * tests
    
    * templates
    
    * tests passing
    
    * remove comments
    
    * remove plugin

---
+++ djangoplicity/releases/tests.py
@@ -0,0 +1,36 @@
+from django.test import TestCase
+from djangoplicity.releases.options import ReleaseOptions
+
+class ReleasesTestCase(TestCase):
+    conf = {
+        'releases': {
+            'root': '/news/',
+            'options': ReleaseOptions,
+            'list_views': [
+                ('embargo', '', 302),
+                ('search', '', 302),
+                ('staging', '', 302),
+                ('year', '2020/', 200),
+            ]
+        }
+    }
+
+    def test_index_root(self):
+        response = self.client.get('/news/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_queries(self):
+        for conf in self.conf.values():
+            options = conf['options']
+            root = conf['root']
+            views = conf['list_views']
+
+            for query, subpart, code in views:
+                view_url_root = "%sarchive/%s/%s" % (root, query, subpart)
+                response = self.client.get(view_url_root)
+                self.assertEqual(response.status_code, code)
+
+                for browser in getattr(options.Queries, query).browsers:
+                    view_url = "%s%s/" % (view_url_root, browser)
+                    response = self.client.get(view_url)
+                    self.assertEqual(response.status_code, code)
