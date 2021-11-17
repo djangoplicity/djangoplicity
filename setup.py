1bb77d44fac3293b99de257806cecad5eee9cad3
Edison Arango <edisonarab@gmail.com>
2021-11-17 10:55:18 -0500

    Feature/upgrade python AVM library

--- setup.py
+++ setup.py
@@ -44,7 +44,7 @@ setup(
         'pika',
         'openpyxl',
         'django-tinymce4-lite == 1.7.2',
-        'python-avm-library @ git+https://github.com/esoobservatory/python-avm-library.git@master#subdirectory=libavm'
+        'python-avm-library @ git+https://github.com/djangoplicity/python-avm-library.git@master#subdirectory=libavm'
     ],
     # metadata for upload to PyPI
     author='European Southern Observatory',
