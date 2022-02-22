23542938abeb921d88162d6ad1206da8db7c7174
Edwin Moreno <edwinmoreno328@gmail.com>
2022-02-22 14:26:29 -0500

    add tests to archive base rename, delete and move

--- djangoplicity/archives/tests.py
+++ djangoplicity/archives/tests.py
@@ -4,10 +4,10 @@
 # Copyright 2007-2008 ESA/Hubble
 #
 #
+from datetime import datetime, timedelta
 from django.test import override_settings
-
 import djangoplicity.archives as package
-from djangoplicity.announcements.models import Announcement, AnnouncementImage
+from djangoplicity.announcements.models import Announcement, AnnouncementImage, AnnouncementProxy
 from djangoplicity.archives.base import resource_deletion_handler, ArchiveModel
 from djangoplicity.archives.resources import ImageResourceManager
 from django.db import models
@@ -17,10 +17,11 @@ from django.http import HttpRequest
 from django.template import RequestContext, Template
 import os
 from djangoplicity.archives.contrib import types
+
 try:
-    from mock import patch, MagicMock
+    from mock import patch, MagicMock, PropertyMock
 except ImportError:
-    from unittest.mock import patch, MagicMock
+    from unittest.mock import patch, MagicMock, PropertyMock
 
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -132,15 +133,30 @@ class ArchiveBaseTestCase(BasicTestCase):
         self.assertTrue(os_make_dir_mock.called)
         self.assertTrue(os_move_file_mock.called)
 
+    @override_settings(USE_I18N=True)
     def test_rename_archive(self):
         # validate archive rename
         instance = Image.objects.get(id='image-1')
+        ann = Announcement.objects.get(id='test-ann')
+        # Add Translations
+        translation = {
+            'id': 'test-ann-es',
+            'source': ann,
+            'published': True,
+            'lang': 'es',
+            'description': 'this is a translation',
+            'subtitle': 'Translation subtitle',
+            'title': 'Ann test translate',
+            'translation_ready': True
+        }
+        AnnouncementProxy.objects.create(**translation)
+
         with self.assertRaises(Exception):
             instance.rename(new_pk='image-2')
 
-        instance.rename(new_pk='new-test-ann-image')
-        ann = Announcement.objects.get(id='test-ann')
-        ann_image = ann.announcementimage_set.get(archive_item_id='new-test-ann-image')
+        instance.rename(new_pk='test-ann-image')
+
+        ann_image = ann.announcementimage_set.get(archive_item_id='test-ann-image')
         self.assertIsInstance(ann_image, AnnouncementImage)
 
         # When rename an announcement or press release the related resources the id must be start with the new pk
@@ -149,3 +165,60 @@ class ArchiveBaseTestCase(BasicTestCase):
 
         related_announcement = instance.announcement_set.get(id='new-test-ann')
         self.assertIsInstance(related_announcement, Announcement)
+
+    @patch('shutil.rmtree')
+    def test_delete_resources(self, delete_file_mock):
+        """ Delete all resources """
+        with patch('djangoplicity.media.models.images.Image.resource_original', new_callable=PropertyMock) as patched_property:
+            class FileResource:
+                def __init__(self):
+                    self.path = 'archives/original/test_file.tiff'
+                    self.name = 'test_file.tiff'
+
+            patched_property.return_value = FileResource()
+            instance = Image.objects.get(id='image-1')
+
+            instance.delete_resources()
+            self.assertTrue(delete_file_mock.called)
+            delete_file_mock.assert_called_with('archives/original/test_file.tiff')
+
+    @patch('os.rename')
+    def test_move_resources(self, os_rename_mock):
+        """ Move all resources """
+        with patch('djangoplicity.media.models.images.Image.resource_original', new_callable=PropertyMock) as patched_property:
+            class FileResource:
+                def __init__(self):
+                    self.path = 'archives/original/test_file.tiff'
+                    self.name = 'test_file.tiff'
+
+            patched_property.return_value = FileResource()
+            instance = Image.objects.get(id='image-1')
+
+            instance.move_resources('image-1')
+            self.assertTrue(os_rename_mock.called)
+
+    @patch('djangoplicity.archives.contrib.social.tasks.facebook_refresh.delay')
+    @patch('djangoplicity.archives.contrib.security.StaticFilesProtectorCache.run_async')
+    def test_archive_miscellaneous_methods(self, static_files_protector_cache_mock, facebook_refresh_mock):
+        instance = Image.objects.get(id='image-1')
+        # Test List of resources that should not be overwritten
+        self.assertEqual(instance.locked_resources(), [])
+        instance.keep_newsfeature = True
+        locked_resources = instance.locked_resources()
+        self.assertEqual(locked_resources, ['banner1920', 'newsfeature', 'potwmedium', 'thumb350x'])
+
+        # Test Called at the time defined by embargo_date
+        instance.embargo_date_action()
+        self.assertTrue(static_files_protector_cache_mock.called)
+
+        # Test Called at the time defined by release_date
+        instance.release_date_action()
+        self.assertTrue(facebook_refresh_mock.called)
+        facebook_refresh_mock.assert_called_with('example.com/images/image-1/')
+
+        # Test the task to run embargo_date_action
+        instance.embargo_task_id = 'a85bedd6-37aa-4363-b149-93f0c18ed159'
+        instance.embargo_date = datetime.now() + timedelta(weeks=1)
+        instance.set_embargo_date_task()
+
+        self.assertNotEqual(instance.embargo_task_id, 'a85bedd6-37aa-4363-b149-93f0c18ed159')
