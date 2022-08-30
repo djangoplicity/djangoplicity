ac66d5951ca40b78b846d7d866c4c070cf9e896b
Alejandro Vivas <alejo.vivas.av@gmail.com>
2022-08-30 08:36:10 -0500

    Added prefix https to urls

--- djangoplicity/crosslinks/__init__.py
+++ djangoplicity/crosslinks/__init__.py
@@ -12,10 +12,10 @@ ARCHIVE_CROSSLINKS = {
         ('esahubble.org', 'https://esahubble.org/news/'),
     ),
     'images': (
-        ('esahubble.org', 'http://www.esahubble.org/images/'),
+        ('esahubble.org', 'https://esahubble.org/images/'),
     ),
     'videos': (
-        ('esahubble.org', 'http://www.esahubble.org/videos/'),
+        ('esahubble.org', 'https://esahubble.org/videos/'),
     ),
     'potw': (
         ('esahubble.org', 'https://esahubble.org/images/potw/'),
@@ -24,41 +24,41 @@ ARCHIVE_CROSSLINKS = {
         ('esahubble.org', 'https://esahubble.org/products/print_posters/'),
     ),
     'books': (
-        ('esahubble.org', 'http://www.esahubble.org/about/further_information/books/'),
+        ('esahubble.org', 'https://esahubble.org/about/further_information/books/'),
     ),
     'brochures': (
-        ('esahubble.org', 'http://www.esahubble.org/about/further_information/brochures/'),
+        ('esahubble.org', 'https://esahubble.org/about/further_information/brochures/'),
     ),
     'calendars': (
-        ('esahubble.org', 'http://www.esahubble.org/products/calendars/'),
+        ('esahubble.org', 'https://esahubble.org/products/calendars/'),
     ),
     'education': (
-        ('esahubble.org', 'http://www.esahubble.org/projects/anniversary/educational_material/'),
+        ('esahubble.org', 'https://esahubble.org/projects/anniversary/educational_material/'),
     ),
     'newsletters': (
         ('esahubble.org', 'https://esahubble.org/newsletters/'),
 
     ),
     'periodicals': (
-        # ( 'esahubble.org', 'http://www.esahubble.org/about/further_information/newsletters/' )#brokend
+        # ( 'esahubble.org', 'https://esahubble.org/about/further_information/newsletters/' )#brokend
     ),
     'postcards': (
         ('esahubble.org', 'https://esahubble.org/products/postcards/'),
     ),
     'logos': (
-        ('esahubble.org', 'http://www.esahubble.org/products/logos/'),
+        ('esahubble.org', 'https://esahubble.org/products/logos/'),
     ),
     'conferenceposters': (
-        ('esahubble.org', 'http://www.esahubble.org/products/conf_posters/'),
+        ('esahubble.org', 'https://esahubble.org/products/conf_posters/'),
     ),
     'presentations': (
-        ('esahubble.org', 'http://www.esahubble.org/products/presentations/'),
+        ('esahubble.org', 'https://esahubble.org/products/presentations/'),
     ),
     'exhibitions': (
-        ('esahubble.org', 'http://www.esahubble.org/products/exhibitions/'),
+        ('esahubble.org', 'https://esahubble.org/products/exhibitions/'),
     ),
     'dvds': (
-        ('esahubble.org', 'http://www.esahubble.org/products/media/'),
+        ('esahubble.org', 'https://esahubble.org/products/media/'),
     ),
     'techdocs': (
         # ( 'esahubble.org', 'http://www.esahubble.org/about/further_information/techdocs/' )#roto
