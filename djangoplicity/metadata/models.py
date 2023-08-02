f8efa58f25ab26edc88065ce1fe4edcd1faefbd9
Edison Arango <edisonarab@gmail.com>
2023-08-02 12:05:14 +0200

    Fix/programs migrations, remove programs type in favor of types

--- djangoplicity/metadata/models.py
+++ djangoplicity/metadata/models.py
@@ -346,7 +346,6 @@ class Program(models.Model):
     Model for storing Programs
     """
     url = models.SlugField( db_index=True, blank=False, null=False, verbose_name=_("URL"), )
-    type = models.ForeignKey( CategoryType, help_text=_("Defines to which archive this query applies."), on_delete=models.CASCADE )
     name = models.CharField( max_length=255, blank=False, null=False, help_text=_("Title of query to be displayed to the user.") )
     types = models.ManyToManyField(CategoryType, help_text=_("Defines to which types this program applies."), related_name='+')
     logo_url = models.URLField(verbose_name="Logo URL", blank=True, null=True, max_length=255)
