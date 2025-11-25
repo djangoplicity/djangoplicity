95d2071a6b3575f22c5dcb432a1a62333fce77c8
Edison Arango <edisonarab@gmail.com>
2025-11-25 11:20:34 -0500

    Revert "Temp/disable S3 tagging while Policy is updated"
    This reverts commit ac1306912798dffdf6a2b8be9f9ee4afa3f600ff.

--- djangoplicity/contentserver/base.py
+++ djangoplicity/contentserver/base.py
@@ -295,8 +295,7 @@ class S3ContentServer(ContentServer):
                 else:
                     logger.warning('S3ContentServer: No access tag found for %s', instance)
 
-                # extra_args = {'ContentType': content_type, 'Tagging': f'Access={access_tag}'}
-                extra_args = {'ContentType': content_type}
+                extra_args = {'ContentType': content_type, 'Tagging': f'Access={access_tag}'}
 
                 # upload the file with the tag included
                 self.s3_client.upload_file(resource.path, self.bucket, remote_path, ExtraArgs=extra_args)
