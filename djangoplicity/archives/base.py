cc4d8acc53a1c35b10d550885e8071411d17007e
Juan D. Gonzalez. <59491445+jdgonzalez03@users.noreply.github.com>
2026-05-12 23:10:17 -0500

    Fix/logic error in get_access_tag_for_format method (#198)
    * Fix/update content server check in ArchiveModel to prevent access tag issues
    
    * Fix/check for attribute existence in content server before accessing always_public_formats

--- djangoplicity/archives/base.py
+++ djangoplicity/archives/base.py
@@ -821,11 +821,12 @@ class ArchiveModel( with_metaclass(ArchiveBase, object) ):
         is_embargoed = self.is_embargoed()
         
         content_server = MEDIA_CONTENT_SERVERS[self.content_server]
-        if not content_server and not content_server.always_public_formats:
+        if not content_server:
             return AccessTagControl.PUBLIC
 
-        if is_embargoed and fmt in content_server.always_public_formats:
-            return AccessTagControl.PUBLIC
+        if is_embargoed and hasattr(content_server, 'always_public_formats'):
+            if fmt in content_server.always_public_formats:
+                return AccessTagControl.PUBLIC
 
         return base_access_tag
 
