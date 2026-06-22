54a2d72ab9e8d290f384a564e7493395889b6cf3
dmartinez <dialmy2094@gmail.com>
2026-06-22 14:53:52 -0500

    fix: Prioritize 2k dome videos for YouTube upload

--- djangoplicity/media/models/videos.py
+++ djangoplicity/media/models/videos.py
@@ -92,8 +92,8 @@ class Video( ArchiveModel, TranslationModel, ContentDeliveryModel ):
 
     UPLOAD_FORMATS = (
         'vr_8k', 'vr_4k', 'cylindrical_preview', 'ultra_hd',
-        'hd_1080p25_screen', 'hd_1080_screen', 'dome_preview', 'hd_broadcast_720p25',
-        'dome_2kplayback', 'dome_4kplayback', 'hd_and_apple', 'medium_podcast',
+        'hd_1080p25_screen', 'hd_1080_screen', 'hd_broadcast_720p25', 'dome_2kplayback',
+        'dome_4kplayback', 'dome_preview', 'hd_and_apple', 'medium_podcast',
         'ext_highres', 'ext_playback', 'old_video', 'vr_16kmaster', 'vr_8kmaster',
         'vr_4kmaster'
     )
